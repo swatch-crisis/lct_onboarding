1d10d81d97ea88c6c5d431e51aa3b2d5c6223b29
Swaleh Mwanza <swalehmwanza@gmail.com>
2024-04-13 13:45:37 +0300

    url change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="lctafrica.net"
+          href="https://lctafrica.net"
           target="_blank"
           rel="noopener noreferrer"
         >
