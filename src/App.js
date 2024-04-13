da5e7a66a29af81e017b949686d77b9f7d9725b4
Swaleh Mwanza <swalehmwanza@gmail.com>
2024-04-13 13:36:00 +0300

    SWATCH MEDIA

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       BRITAM INSURANCE COMPANY
+       SWATCH MEDIA
         </p>
         <a
           className="App-link"
