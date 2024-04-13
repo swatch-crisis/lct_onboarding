36409247a9ba873f62e79da27468af4d7a5c5ee0
Swaleh Mwanza <swalehmwanza@gmail.com>
2024-04-13 13:42:07 +0300

    lct africa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-       SWATCH MEDIA
+       LCT AFRICA
         </p>
         <a
           className="App-link"
