b6133985224c627b1dc85496f87c22132925be0d
Swaleh Mwanza <swalehmwanza@gmail.com>
2024-04-13 13:31:57 +0300

    line changes

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="swatch-media@vercel.app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Swatch Media
         </a>
       </header>
     </div>
